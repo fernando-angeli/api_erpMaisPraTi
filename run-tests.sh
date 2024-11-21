a9612cb13093c2ee65aa3b52672ceae66eaffa14
fernando-angeli <fernando_angeli@hotmail.com>
2024-11-21 09:45:08 -0300

    update docker-compose.yml and run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-echo "Rodando os testes..."
+export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=512m"
+mvn clean test
+
+echo "Iniciando os testes..."
+
+# Rodar os testes com Maven
 mvn test
+
+# Capturar o status dos testes
+if [ $? -eq 0 ]; then
+    echo "Testes concluídos com sucesso!"
+    exit 0
+else
+    echo "Falha nos testes."
+    exit 1
+fi
